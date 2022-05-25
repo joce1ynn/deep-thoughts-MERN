8864f494e77436efeef753dfbd6134b1b3f1c831
Wenwen Tian <wwtian9@gmail.com>
2022-05-25 16:16:06 -0600

    add thought list

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React from "react";
+
+// integrate apollo useQuery hook
+// make requests to the GraphQL server and made available to use the <ApolloProvider> component in App.js earlier.
+import { useQuery } from "@apollo/client";
+// show thoughts on home page
+import { QUERY_THOUGHTS } from "../utils/queries";
 
 const Home = () => {
+  // use useQuery hook to make query request
+  // When loading the Home component, we'll execute the query for the thought data
+  // data returned from the server, that information is stored in the destructured data property.
+  const { loading, data } = useQuery(QUERY_THOUGHTS);
+
+  //because GraphQL response comes in a big data object. just need to access data.thoughts
+  // ? is optional chaining, it doesn't need to check if an object exists before accessing its properties
+  // no data will exist until the query to the server is finished
+  // if data exists, store it in the thoughts constant we just created.
+  // If data is undefined, then save an empty array to the thoughts component.
+  const thoughts = data?.thoughts || [];
+  console.log(thoughts);
+
   return (
     <main>
-      <div className='flex-row justify-space-between'>
-        <div className='col-12 mb-3'>{/* PRINT THOUGHT LIST */}</div>
+      <div className="flex-row justify-space-between">
+        <div className="col-12 mb-3">{/* PRINT THOUGHT LIST */}</div>
       </div>
     </main>
   );
---
+++ client/src/utils/queries.js
@@ -0,0 +1,20 @@
+//wrapped query code in a tagged template using the imported gql function
+import { gql } from "@apollo/client";
+
+export const QUERY_THOUGHTS = gql`
+  query thoughts($username: String) {
+    thoughts(username: $username) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+        createdAt
+        username
+        reactionBody
+      }
+    }
+  }
+`;
