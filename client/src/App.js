0e5b9587711adc2fdbe6185ae55d88b2bd3cae75
Wenwen Tian <wwtian9@gmail.com>
2022-05-28 10:40:02 -0600

    add react router

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,9 @@ import {
   createHttpLink,
 } from "@apollo/client";
 
+// We renamed BrowserRouter to Router to make it easier to work with.
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 // ApolloProvider is a special type of React component that we'll use to provide data to all of the other components.
 // ApolloClient is a constructor function that will help initialize the connection to the GraphQL API server.
 // InMemoryCache enables the Apollo Client instance to cache API response data so that we can perform requests more efficiently.
@@ -13,7 +16,13 @@ import {
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import NoMatch from "./pages/NoMatch";
+import SingleThought from "./pages/SingleThought";
+import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
 
 // establish a new link to the GraphQL server
 const httpLink = createHttpLink({
@@ -32,15 +41,25 @@ const client = new ApolloClient({
 
 function App() {
   //wrap the entire returning JSX code with <ApolloProvider>
+  // a path that leads to the wildcard character, an asterisk (*) sending the 404 message.
   return (
     <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        <div className="container">
-          <Home />
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/thought" element={<SingleThought />} />
+              <Route path="*" element={<NoMatch />} />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
