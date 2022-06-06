0c25e0a991af184c1fd946c4f9189358b4b58f59
Wenwen Tian <wwtian9@gmail.com>
2022-06-06 12:03:03 -0600

    add friend button

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,16 +2,20 @@ import React from "react";
 import ThoughtList from "../components/ThoughtList";
 import FriendList from "../components/FriendList";
 import { QUERY_USER, QUERY_ME } from "../utils/queries";
+import { ADD_FRIEND } from "../utils/mutations";
 
 //retrieves the username from the URL,
 import { Navigate, useParams } from "react-router-dom";
-import { useQuery } from "@apollo/client";
+import { useQuery, useMutation } from "@apollo/client";
 
 // compare the username parameter in the URL to the logged-in user with our AuthService functionality.
 import Auth from "../utils/auth";
 
 const Profile = (props) => {
   const { username: userParam } = useParams();
+
+  //destructure the mutation function from ADD_FRIEND so we can use it in a click function
+  const [addFriend] = useMutation(ADD_FRIEND);
 
   // pass username from url to the useQuery Hook
   // if there's a value in userParam that we got from the URL bar, we'll use that value to run the QUERY_USER query
@@ -42,12 +46,29 @@ const Profile = (props) => {
     );
   }
 
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id },
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  // userParam is only defined when the route includes a username, so users cant be friends with themselves
   return (
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
           Viewing {userParam ? `${user.username}'s` : "your"} profile.
         </h2>
+
+        {userParam && (
+          <button className="btn ml-auto" onClick={handleClick}>
+            Add Friend
+          </button>
+        )}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
