a046d2b201b671cd04a00f5c57770c34e749c1c7
Wenwen Tian <wwtian9@gmail.com>
2022-06-08 11:51:06 -0600

    update thought when submitting

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -2,12 +2,44 @@ import React, { useState } from "react";
 import { ADD_THOUGHT } from "../../utils/mutations";
 import { useMutation } from "@apollo/client";
 
+//update the cache of any related queries
+// The Apollo Client tracks cached objects by their IDs.
+// In this case, we've added a new thought that should go inside an array of thoughts,
+// but the array itself has no ID to track.
+//So the only way to get the updated array is to re-request it from the server.
+import { QUERY_THOUGHTS, QUERY_ME } from "../../utils/queries";
+
 const ThoughtForm = () => {
   const [thoughtText, setText] = useState("");
   const [characterCount, setCharacterCount] = useState(0);
 
-  // add thoughts
-  const [addThought, { error }] = useMutation(ADD_THOUGHT);
+  // the addThought() function will run the actual mutation.
+  // The error variable will initially be undefined but can change depending on if the mutation failed.
+  const [addThought, { error }] = useMutation(ADD_THOUGHT, {
+    update(cache, { data: { addThought } }) {
+      // could potentially not exist yet, so wrap in a try/catch
+      try {
+        // update me array's cache
+        const { me } = cache.readQuery({ query: QUERY_ME });
+        cache.writeQuery({
+          query: QUERY_ME,
+          // update thoughts in me query
+          data: { me: { ...me, thoughts: [...me.thoughts, addThought] } },
+        });
+      } catch (e) {
+        console.warn("First thought insertion by user!");
+      }
+
+      //read what's currently in the cache
+      const { thoughts } = cache.readQuery({ query: QUERY_THOUGHTS });
+
+      // add the newest thought to the front of the array
+      cache.writeQuery({
+        query: QUERY_THOUGHTS,
+        data: { thoughts: [addThought, ...thoughts] },
+      });
+    },
+  });
 
   // form content
   const handleChange = (event) => {
@@ -22,15 +54,27 @@ const ThoughtForm = () => {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
-    // clear the form values
-    setText("");
-    setCharacterCount(0);
+    try {
+      // add thoughts to database
+      await addThought({
+        variables: { thoughtText },
+      });
+
+      // clear the form values
+      setText("");
+      setCharacterCount(0);
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return (
     <div>
-      <p className={`m-0 ${characterCount === 280 ? "text-error" : ""}`}>
+      <p
+        className={`m-0 ${characterCount === 280 || error ? "text-error" : ""}`}
+      >
         Character Count: {characterCount}/280
+        {error && <span className="ml-2">Something went wrong...</span>}
       </p>
       <form
         className="flex-row justify-center justify-space-between-md align-stretch"
